bad2414b36ac91d3e4b92505bb58d5a5fbc5d4c3
Kyle Zhou <kylezhou2002@hotmail.com>
2018-07-09 23:31:56 -0700

    script now runs slightly later

--- content.js
+++ content.js
@@ -1,5 +1,19 @@
-const previews = document.getElementsByClassName('preview');
-const dbutton = document.getElementsByClassName('btn-download')[0];
+window.addEventListener("load", main, false);
 
-previews[0].click()
-alert(dbutton);
+function main(){
+    let previews, dbutton;
+    var jsInitChecktimer = setInterval(checkLoaded, 111);
+
+    function checkLoaded(){
+        let previews = document.getElementsByClassName('preview');
+        let dbutton = document.getElementsByClassName('btn-download')[0];
+        console.log(dbutton)
+        
+        if(previews && dbutton){
+            clearInterval(jsInitChecktimer);
+            console.log(previews);
+            previews[0].click();
+            console.log(dbutton.href)
+        }
+    }
+}
